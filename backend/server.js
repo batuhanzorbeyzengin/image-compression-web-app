98727db3757e7d70132c2efa1b5cc2ab3ccd6959
Batuhan Zorbey Zengin <batuhanzorbeyzengin@gmail.com>
2022-12-25 22:13:20 +0300

    server.js file update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.use(express.json({limit: '50mb'})); // {limit: '50mb'}
+app.use(express.json({limit: '50mb'}));
 const cors = require('cors');
 
 app.use(cors());
